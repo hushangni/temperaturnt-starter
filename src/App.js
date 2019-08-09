bc01369c8714bfadb92c3ec467332ff619c7bab3
Shangni <shangnihu@gmail.com>
2019-08-09 17:13:55 -0400

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import flame from './assets/gradient-flame.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './styles/setup.css';
+import './styles/App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      city: '',
+    }
+  }
+
+  handleChange = (e) => {
+    const userInput = e.target.value;
+    this.setState({
+      city: userInput
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.fetchWeather(this.state.city);
+  }
+
+  async fetchWeather(q) {
+    try {
+
+      const apiData = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
+        params: {
+          appid: 'aab53c547c655e0fb456f3d6f933d4b0',
+          q: q,
+          units: 'metric'
+        }
+      });
+      console.log(apiData);
+
+      this.setState({
+        temperature: apiData.data.main.temp,
+        weather: apiData.data.weather[0].main
+      })
+
+    } catch(err) {
+
+      this.setState({
+        error: err
+      })
+
+    }
+  }
+
+  render() {
+    const { temperature, weather }  = this.state;
+    return (
+      <main className="wrapper">
+          <h1>
+            <img src={flame} className="flame" alt="Orange flame icon."/>
+            <span>temperaturnt</span>
+          </h1>
+
+          <form action="#" onSubmit={this.handleSubmit}>
+            <label htmlFor="city">enter a city: </label>
+            <input type="text" id="city" placeholder="toronto" onChange={this.handleChange}/>
+
+            <input type="submit" value="submit"/>
+          </form>
+
+          {
+            temperature ?
+              <div className="results">
+                <p>{temperature}</p>
+                <p>{weather}</p>
+              </div>
+
+              :
+
+              <div className="results">
+                <p>search for the weather in a city!</p>
+              </div>
+          }
+      </main>
+    );
+  }
 }
 
 export default App;
+
---
+++ src/components/Weather.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Weather = () => {
+    return (
+        <div>Loaing weather things</div>
+    );
+};
+
+export default Weather;
